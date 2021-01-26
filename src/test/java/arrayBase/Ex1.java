165a36075e5072eab94c10ab61254735893f28a4
Elyasaf203 <elyasafohayon@gmail.com>
2021-01-26 13:33:17 +0200

    added javadoc

--- src/test/java/arrayBase/Ex1.java
+++ src/test/java/arrayBase/Ex1.java
@@ -1,10 +1,17 @@
 package arrayBase;
 
+import java.util.Arrays;
+
 public class Ex1 {
+    /**
+     * main function of exercise 1
+     * @param args
+     */
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5};
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
+        Arrays.toString(array);
     }
 }
